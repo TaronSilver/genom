02d15b6005c0fdde3747885fd4cc9de4ed12893e
Katia01 <katia.schalk@epfl.ch>
2016-10-09 20:39:20 +0200

    Update Matrix.hpp

--- Matrix.hpp
+++ Matrix.hpp
@@ -2,6 +2,7 @@
 
 // Created by Erica Geneletti and Katia Schalk on 5/10/16
 
+Using namespace std;
 
 class Matrix
 {
@@ -9,5 +10,6 @@ class Matrix
 	public :
 	
 	static double getProbability (char const N, int const l); //returns the probability for a nucleotide in a specific position
-	
+								  //The first argument corresponds to the column: the nucleotide who interest us.
+								  // The second argument corresponds to the lign: the place of this nucleotide in the sequence of 7 nucleotides.
 };
