69f9cc3ee6b0ccdc43574a60495859104c5071dd
Katia01 <katia.schalk@epfl.ch>
2016-10-09 20:41:44 +0200

    Update Matrix.cpp

--- Matrix.cpp
+++ Matrix.cpp
@@ -15,31 +15,31 @@ double Matrix::getProbability (char const N, int const l)
 {
 	if (l<1 or l>7 or N!='A' or N!='T' or N!='G' or N!='C')
 	{	
-        std::cerr << "Error : negative line value or invalid nucleotide character" << std::endl;
+        cerr << "Error : negative line value or invalid nucleotide character" << endl;
     }
 	
-	std::ifstream PPM;
+	ifstream PPM;
 	PPM.open("DBP_PPM.mat");
 	
 	if (PPM.fail())
 	{
-		std::cerr << "Error : Cannot read file DBP_PPM.mat" << std::endl;
+		cerr << "Error : Cannot read file DBP_PPM.mat" << endl;
 		
 		return(1); //what should we return in case of error?
 		
 	} else {
-				std::string line;
+				string line;
 				int i(1);
 				double A, T, C, G;
 				
-				while(i<=l)								//obtains the requested line in the PPM stream
+				while(i<=l)				//obtains the requested line in the PPM stream
 				{
 					getline(PPM, line);	
 					++i;
 				}
 				
-				std::istringstream values(line);		//copies line into new stream
-				values >> A >> C >> G >> T;				//reads new stream and puts values into nucleotide probabilities
+				istringstream values(line);		//copies line into new stream
+				values >> A >> C >> G >> T;		//reads new stream and puts values into nucleotide probabilities
 				
                 PPM.close();
         
