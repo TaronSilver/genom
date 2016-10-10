9a0492f2ee7c96f0e018682129c5c4eb45da798a
tvyvyan <tristan.vyvyan-robinson@epfl.ch>
2016-10-10 15:01:08 +0200

    Update Sequence.hpp

--- Sequence.hpp
+++ Sequence.hpp
@@ -1,6 +1,18 @@
 
 class Sequence
 	{
-		public:
+		private:
+		double baseProbabibilityA;
+		double baseProbabibilityT;
+		double baseProbabibilityC;
+		double baseProbabibilityG;
+		const string sequence;
+		Sequence(const string sequence);//constructor
+		void calc_BaseProb();
+		
+			 public:
+		
+		vector<size_t> find_sequence(const string& string_to_find, const vector<string>& genes, int sequence_in_gene);//TODO
+
 	
 	};
