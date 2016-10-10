21b0dd65b0de0f08bda24bb9ae1142148fc61bdd
Katia01 <katia.schalk@epfl.ch>
2016-10-10 09:42:56 +0200

    Update Matrix.hpp

--- Matrix.hpp
+++ Matrix.hpp
@@ -10,7 +10,9 @@ class Matrix
 	
 	public :
 	
-	static double getProbability (char const N, int const l); //returns the probability for a nucleotide in a specific position
-								  //The first argument corresponds to the column: the nucleotide who interest us.
-								  // The second argument corresponds to the lign: the place of this nucleotide in the sequence of 7 nucleotides.
+	//Returns the probability for a nucleotide in a specific position.
+	static double getProbability (char const N, int const l); 
+	//The first argument corresponds to the column: the nucleotide who interest us.
+	// The second argument corresponds to the lign: the place of this nucleotide in the sequence of 7 nucleotides.
+	
 };
