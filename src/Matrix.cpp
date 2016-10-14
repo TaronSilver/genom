f2fac144ed2c2c7a9b07eaac5083af343b2531ea
Mättu <myfiles@ext-136-247.eduroam.unibe.ch>
2016-10-14 16:46:15 +0200

    Added function to extract the useful sequences. Have not tried to compile yet.

--- src/Matrix.cpp
+++ src/Matrix.cpp
@@ -72,3 +72,75 @@ double Matrix::getProbability (char const N, int const l)
                 //should we put an error message anyway in case there are problems?
             }
 }
+
+
+
+/* Function that tests all combinations of the position weight matrix and fills
+ sequenceList with all sequences with a score higher than the specified cutoff.*/
+
+void Matrix::sequenceExtract(double cutoff) {
+    
+    // Double that sums the current combination
+    double score(0);
+    
+    // Variables that represent matrix properties, to avoid unnecessery
+    // computations at each iteration, increasing performance.
+    unsigned int matrixSize = logMatrix.size();
+    unsigned int matrixLastElement = matrixSize - 1;
+    
+    // String that saves current nucleotide sequence
+    std::string nucleotideSequence = "";
+    
+    // Vector that iterates through all possible combinations of matrices
+    std::vector<int> iterator;
+    for(unsigned int i(0); i < logMatrix.size(); i++) {
+        iterator[i] = 0;
+    }
+    
+    // Vector defining the order of the characters
+    std::vector<char> basePosition;
+    {
+        basePosition[0] = 'A';
+        basePosition[1] = 'C';
+        basePosition[2] = 'G';
+        basePosition[3] = 'T';
+    }
+    
+    
+    while(iterator[0]!=NUMBER_NUCLEOTIDES) {
+        
+        // Adds up scored at specific iteration
+        for(unsigned int i(0); i < matrixSize; i++) {
+            score+= logMatrix[i][iterator[i]];
+        }
+        
+        // Prints all nucleotid combinations that have score larger than cutoff
+        if (score >= cutoff) {
+            for(unsigned int i(0); i < matrixSize; i++) {
+                nucleotideSequence += basePosition[iterator[i]];
+            }
+            
+            sequenceList.push_back(nucleotideSequence);
+            nucleotideSequence = "";
+        }
+        
+        iterator[matrixLastElement]++;
+        
+        for (unsigned int i(matrixLastElement); i>0; i--) {
+            if (iterator[i]==NUMBER_NUCLEOTIDES) {
+                iterator[i-1]++;
+                iterator[i] = 0;
+            }
+        }
+    }
+    
+    return;
+}
+
+/* Function that accesses the extracted DNA sequences and returns a vector of strings. */
+std::vector<std::string> accessDNASequences() {
+    return sequenceList;
+    
+    // It'd be probably good for the function to throw an error if this list is empty
+}
+
--- src/Matrix.hpp
+++ src/Matrix.hpp
@@ -7,20 +7,27 @@
 
 #include <string>
 
+/* Defines number of different nucleotides in DNA sequnece (always 4) */
+#define NUMBER_NUCLEOTIDES 4
+
+
 
 typedef std::array < double,4 > BaseProbabilities;
 typedef std::vector<std::vector<double>> ProbMatrix;
 typedef std::vector<std::array<double, 4>> LogMatrix;
+typedef std::vector<std::string> SequenceList;
 
 class Matrix
 {
 	private:
 	LogMatrix logMatrix;
 	ProbMatrix probMatrix;
+    SequenceList sequenceList;
 	Matrix(const std::string& fileName); // cobstructor
 	void probToLog();
 	// add an atribute sequence ???
 	
+    
 	public :
 	
 	/*Method that compute the logMatrix from the probMatrix.
@@ -30,5 +37,12 @@ class Matrix
 	/* Returns the probability for a nucleotide in a specific position.
 	The first argument corresponds to the column: the nucleotide who interest us.
 	The second argument corresponds to the lign: the place of this nucleotide in the sequence of 7 nucleotides.*/
-	static double getProbability (char const N, int const l); // Remove static when everything works	
+	static double getProbability (char const N, int const l); // Remove static when everything works
+    
+    /* Function that tests all combinations of the position weight matrix and fills
+    sequenceList with all sequences with a score higher than the specified cutoff.*/
+    void sequenceExtract(double cutoff);
+    
+    /* Function that accesses the extracted DNA sequences and returns a vector of strings. */
+    std::vector<std::string> accessDNASequences();
 };
