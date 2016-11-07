eed3b2dccf40fbb21bad4a07997bfce33e58c0ad
Lucas Zweili <lucas.zweili@epfl.ch>
2016-11-07 14:26:56 +0100

    final commit

--- src/Sequence.cpp
+++ src/Sequence.cpp
@@ -19,21 +19,17 @@ std::vector<size_t> Sequence::find_sequence(const std::string& string_to_find) /
     std::string Translated_String(TranslateSeq(string_to_find));
     
     // std::cout << "SEARCHING '" << string_to_find << "' in the sequence"<< std::endl;
-    
+   
     do {
         starting_position = sequence.find(string_to_find, starting_position+1); //the function "find" returns the first position of the first character of the first match
         if(starting_position != std::string::npos)
         {
             starting_positions.push_back(starting_position);
-            std::cout << string_to_find << " starting at char " << starting_position <<" +"<< std::endl; //translated_string équivalent au binding spot
+            std::cout << string_to_find << " starting at char " << starting_position+1 <<" +"<< std::endl; //translated_string équivalent au binding spot
         }
     } while (starting_position != std::string::npos);
     
 
-    return starting_positions;
-    
-    /*
-     Doesnt work yet
      
     starting_position = -1;
     
@@ -42,14 +38,15 @@ std::vector<size_t> Sequence::find_sequence(const std::string& string_to_find) /
         if(starting_position != std::string::npos)
         {
             starting_positions.push_back(starting_position);
-            std::cout << Translated_String << " starting at char " << starting_position <<" -"<< std::endl; //string_to_find équivalent au binding spot
+            std::cout << string_to_find << " starting at char " << starting_position+1  <<" -"<< std::endl; //string_to_find équivalent au binding spot
         }
     } while (starting_position != std::string::npos);
     
-    
+    /*
     std::cout << " SCAN FINISHED " << starting_positions.size() << " occurences found" << std::endl;
-    return starting_positions;
     */
+    return starting_positions;
+  
 }
 
 
