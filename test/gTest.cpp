bc02d21a8af8b84ece0d177637d1ecd0a9453d7d
angelasaudan <angela.saudan@epfl.ch>
2016-11-21 14:38:34 +0100

    Two more gtests

--- test/gTest.cpp
+++ test/gTest.cpp
@@ -1,5 +1,10 @@
 #include "gtest/gtest.h"
 #include "../src/Matrix.hpp"
+#include "../src/utility.hpp"
+#include "../src/Sequence.hpp"
+#include <iostream>
+#include <string>
+#include <fstream>
 
 /*! I use the constructor to initialise the four 4 different matrix.
  * This matrix will be use to check if our function work as expected because 
@@ -18,12 +23,27 @@ Matrix ma_matrice_3(lm,logMatrix);
 Matrix_Neo lcm({{-1.947529,MINUSINFINI,-2.754889,0.000}});
 Matrix ma_matrice_4(lcm,logConstMatrix);
 
-SimpleVector value_test_1({1.407408});
-SimpleVector value_test_2({0.710526});
-SimpleVector value_test_3({1.40741});
-SimpleVector value_test_4({1.506960});	
+std::vector<double> value_test_1({1.407408});
+std::vector<double> value_test_2({0.710526});
+std::vector<double> value_test_3({1.40741});
+std::vector<double> value_test_4({1.506960});	
 
-BaseProbabilities BP({0.25,0.25,0.25,0.25}); 		
+std::vector<double> BP({{0.25,0.25,0.25,0.25}});
+
+std::vector<std::string> nucleotide_sequences ({{"AACGT", "CCGTA", "AATCG", "CCGTA"}});
+unsigned int position = 2;
+
+Sequence one ("AACG");
+Sequence two ("CCGT");
+Sequence three ("AATC");
+Sequence four ("CCGT");
+std::vector<Sequence> sequence_list ({one, two, three, four});
+bool entire_sequence = true;
+
+std::vector<double> value_test_5 ({0.0,0.25,0.5,0.25});
+
+Matrix_Neo value_test_6 ({{0.5,0.5,0.0,0.0},{0.5,0.5,0.0,0.0},{0.0,0.25,0.5,0.25},{0.0,0.25,0.25,0.5}});
+
 /*!
  *@brief Function testing if calcul_sum() returns the expected value, using a relativematrix
  */
@@ -119,6 +139,8 @@ TEST (compute_logConstMatrix_from_logMatrix_test, goodlogConstMatrix)
 
 
 
+//-------------------------------------------- gTests Failed
+
 /*!
  *@brief Function testing if compute_abs_relativeMatrix() returns the good relativeMatrix
  */
@@ -143,8 +165,7 @@ TEST (compute_rel_absoluteMatrix_test, goodabsoluteeMatrix)
 	{
 	ASSERT_TRUE(std::abs(a[0][j]-ma_matrice_1.compute_rel_absoluteMatrix()[0][j]) < 0.0001);
 	}
-}
-//-------------------------------------------- gTests Failed
+} 
 
 /*!
  *@brief Function testing if compute_logConstMatrix_from_relativeMatrix() returns the good logConstMatrix
@@ -157,6 +178,21 @@ TEST (compute_logConstMatrix_from_relativeMatrix_test, goodlogConstMatrix)
 	}
 } 
 
+/*!
+ *@brief Function testing if "nucleotide_probability" returns the expected value, using nucleotides sequences and a position number
+ */
+TEST (nucleotide_probability_Test, Good_nucleotide_probability)
+{
+	ASSERT_EQ(value_test_5,nucleotide_probability(nucleotide_sequences, position));
+}
+
+/*!
+ *@brief Function testing if "generate_PWM_from_Seq_list" returns the expected value, using nucleotides sequences and a bool variable
+ */
+TEST (generate_PWM_from_Seq_list_Test, PWM)
+{
+	ASSERT_EQ(value_test_6,generate_PWM_from_Seq_list(sequence_list, entire_sequence));
+}
 
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
